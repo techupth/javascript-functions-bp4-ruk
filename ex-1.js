b8cab136a02b4ade365731ab85adef8ddb311f83
Anuruk <anuruk.chalerm@gmail.com>
2024-06-06 01:27:29 +0700

    send answer

--- ex-1.js
+++ ex-1.js
@@ -1,1 +1,24 @@
 // Start coding here
+function add(a,b) {
+    return a + b
+};
+
+let subtract = (a,b) => {
+    return a - b
+};
+
+let multiply = function (a,b) {
+    return a * b
+};
+
+let divide = (a,b) => a / b;
+
+let add1 = add(10,5);
+let subtract1 = subtract(10,5);
+let multiply1 = multiply(10,5);
+let divide1 = divide(10,5);
+
+console.log(add1);
+console.log(subtract1);
+console.log(multiply1);
+console.log(divide1);
--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,26 @@
 // Start coding here
+let add = function (a,b) {
+    return a + b
+};
+
+let subtract = function (a,b) {
+    return a - b
+};
+
+let multiply = function (a,b) {
+    return a * b
+};
+
+let divide = function (a,b) {
+    return a / b
+};
+
+let add1 = add(10,5);
+let subtract1 = subtract(10,5);
+let multiply1 = multiply(10,5);
+let divide1 = divide(10,5);
+
+console.log(add1);
+console.log(subtract1);
+console.log(multiply1);
+console.log(divide1);
